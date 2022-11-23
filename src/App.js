fa1129c0207cdcf263f1b7c7b0a199e91b523791
Strong10mede <kmayur809@gmail.com>
2022-11-24 00:38:51 +0530

    app.js done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Feed from "./Components/Feed/Feed";
+import Widgets from "./Components/Widgets/Widgets";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
+  );
 }
 
 export default App;
